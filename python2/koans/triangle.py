0de23b4c3e8fabebd097643213a327411e364107
Harish Narayanan <hnarayanan@gmail.com>
2013-03-21 15:05:38 +0100

    Make initial triangle project tests pass

--- python2/koans/triangle.py
+++ python2/koans/triangle.py
@@ -18,9 +18,12 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
-
+    if a == b == c:
+        return 'equilateral'
+    elif a == b != c or b == c != a or c == a != b:
+        return 'isosceles'
+    else:
+        return 'scalene'
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(StandardError):
